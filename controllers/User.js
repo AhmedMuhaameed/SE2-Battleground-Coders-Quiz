15edfad640784a719141b33e03cb9e74755d2838
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2019-04-09 18:30:08 +0200

    Add files via upload

---
+++ controllers/User.js
@@ -0,0 +1,12 @@
+function User(first_name,last_name,id,password,email,points,results,quizzes_list)
+{
+  	this.id = id,
+    this.first_name = first_name,
+    this.last_name = last_name,
+    this.password = password,
+    this.email = email,
+    this.points = points,
+    this.results = results,
+    this.quizzes_list = quizzes_list
+}
+
