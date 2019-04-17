3a304a3f9a6fd13080e47f494addf097ee9a72c3
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2019-04-17 23:01:41 +0200

    Update quizController.js

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,9 +1,19 @@
 var bodyParser = require('body-parser');
 const quizModel = require('../models/quizModel');
 
-var data = [{ question: 'what\'s your name? ', choice: ['Ehab', 'Magdy', 'Abd El-Rahman'], correct: 0 },
-{ question: 'what\'s your age? ', choice: ['20', '19', '21'], correct: 2 },
-{ question: 'How are you', choice: ['Fine', 'Good', 'Upset'], correct: 1 }];
+var data = {
+    field: 'CS',
+    size: 3, 
+    question: ['what\'s your name? ', 'what\'s your age? ', 'How are you ?'], 
+    choices: [['Ehab', 'Magdy', 'Abd El-Rahman'], ['20', '19', '21'], ['Fine', 'Good', 'Upset']], 
+    answer: ['Ehab', '21', 'Good']
+};
+
+function clone(a) 
+{
+    return JSON.parse(JSON.stringify(a));
+}
+
 var answer = [];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -12,28 +22,108 @@ module.exports = function (app) {
     app.use(bodyParser.json()); // to support JSON bodies
     app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-    app.get('/quiz', function (req, res) {
-        res.render('quiz', { questions: data });
-        console.log("again");
-    });
 
-    app.get('/quiz_success', function (req, res) {
-        console.log("IN GET-Quiz_Success");
-        res.send(answer);
+    app.get('/test', function (req, res) {
+
+        const node = new quizModel({
+            size: 3,
+            field: "Asd",
+            question: ["zxc", "q2", 'q3'],
+            choices: [["qwe", "Qasdssde"], ["C1", "C2", "c3"]],
+            answer: ["ZXC", "Qasdssde", "C3"]
+        });
+
+        const initStep = new quizModel({
+            size: data.size,
+            field: data.field,
+            question: data.question,
+            choices: data.choices,
+            answer: data.answer
+        });
+        
+        //console.log(node);
+
+                
+        quizModel.find({}, function(err, res)
+        {
+            if(err) console.error(err);
+            else
+            {
+                console.log("All Quizes")
+                for(var i = 0; i < res.length; i++)
+                {
+                    console.log(res[i]);
+                }
+                data = res;
+            }
+        })
+        console.log("*****************");
+        //console.log(data);
+        res.json(data);
+        
+        /*
+        initStep.save(function (err, x) {
+            if (err) console.error(err);
+            else{
+                console.log("qwewqe");
+                res.json(data);
+            }
+        });
+        */
+          
     })
+
+    app.get('/quiz', function (req, res) {
+        quizModel.find({}, function(err, quizzes)
+        {//5cb24d427f95a010f4d23914
+            if(err) console.error(err);
+            else
+            {
+                console.log("All Quizes")
+                console.log("res == " + quizzes);
+                let quiz = quizzes[Math.floor(Math.random()*quizzes.length)];
+                res.render('quiz', {node: quiz});
+            }
+        })
+    });
 
     app.post('/quiz_success', urlencodedParser, function (req, res) {
         var score = 0;
-        for (var i = 0; i < data.length; i++) {
-            console.log('user answer: ' + req.body[i]);
-            console.log("correct answer: " + data[i].correct);
-            if (data[i].correct == req.body[i]) {
-                score++;
+        console.log("quiz_success");
+        quizModel.find({_id:req.body.id}, function(err, data){
+            if(err) console.error(err);
+            else{
+                console.log("Data with id = ", req.body.id)
+                console.log(data[0]);
+                for (var i = 0; i < data[0].size; i++) {
+                    console.log('user answer: ' + data[0].choices[i][req.body[i]]);
+                    console.log("correct answer: " + data[0].answer[i]);
+                    if (data[0].answer[i] == data[0].choices[i][req.body[i]]) {
+                        score++;
+                    }
+                }
+                console.log("score: " + score);
+                res.json({Score: (score * 1.0 / data[0].size) * 100.0 + "%"});
             }
-        }
-        console.log("score: " + score);
-        res.send((score * 1.0 / data.length) * 100.0 + "%");
+        })
     });
+
+    /*===========================Admin==============*/
+    app.get('/adminQuizController', function(req, res){
+        quizModel.find({}, function(err, quizzes){
+            if(err) console.error(err);
+            else{
+                res.render('AdminQuizController', {node: quizzes});
+            }
+        })
+    })
+
+    app.post('/tempPageForAdmin', function(req, res){
+        console.log("lol")
+        quizModel.deleteOne({_id:req.body.id}, function(err, data){
+            res.redirect('/adminQuizController');
+        })
+    })
 
     app.get('/addQuiz', function (req, res) {
         res.render('addQuiz', { queryString: req.query });
@@ -43,10 +133,33 @@ module.exports = function (app) {
         console.log("req.body: " + req.body);
         if (!req.body) return res.send(404);
         
-        var finalData = [{field: req.body[100]}, { question: req.body[0], choices: [req.body[4], req.body[5], req.body[6]], answer: '' },
-        { question: req.body[1], choices: [req.body[14], req.body[15], req.body[16]], answer: '' },
-        { question: req.body[2], choices: [req.body[24], req.body[25], req.body[26]], answer: '' }];
-        console.log("finalData.length = " + finalData.length);
+        var finalData = {
+            size: 3,
+            field: req.body[100],
+            question: [req.body[0], req.body[1], req.body[2]],
+            choices: [[req.body[4], req.body[5], req.body[6]], [req.body[14], req.body[15], req.body[16]],  [req.body[24], req.body[25], req.body[26]]],
+            answer: [req.body[7], req.body[17], req.body[27]]
+        };
+
+        const add_quiz = new quizModel(
+            {
+                size: 3,
+                field: finalData.field,
+                question: finalData.question,
+                choices: finalData.choices,
+                answer: finalData.answer
+            }
+        )
+
+        add_quiz.save(function(err, x){
+            if(err) console.error(err)
+            else{
+                console.log(finalData);
+                //res.json(finalData);
+            }
+        })
+
+        console.log("finalData.length = " + finalData.size);
         /*
 		quizModel.create(finalData).then(function(req, res){
             res.render('addQuiz_success', { inputData: finalData });
@@ -61,6 +174,5 @@ module.exports = function (app) {
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
         res.json(data);
-
     });
 }
