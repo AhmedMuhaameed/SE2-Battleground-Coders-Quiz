461aad39dcde6dfead74f61661fb67e6bd3b1f5e
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2019-04-29 15:49:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI: 'mongodb+srv://ahmedehab:ahmed@quiz-hvhc1.gcp.mongodb.net/test?retryWrites=true'
 }
-//mongodb://localhost/quiz
+//mongodb://localhost/quiz ==> to connect to offline database caled quiz
 //mongodb+srv://ahmedehab:ahmed@quiz-hvhc1.gcp.mongodb.net/test?retryWrites=true
 //mongodb+srv://AhmedEhab:ahmed123@quizcluster-c3eyb.mongodb.net/test?retryWrites=true
