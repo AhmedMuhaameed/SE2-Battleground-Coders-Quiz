fe689837f9f679cbe0d54ecf4920bb6c99295da1
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2019-04-29 15:49:26 +0200

    connect to online database

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost/quiz'
+    mongoURI: 'mongodb+srv://ahmedehab:ahmed@quiz-hvhc1.gcp.mongodb.net/test?retryWrites=true'
 }
+//mongodb://localhost/quiz
+//mongodb+srv://ahmedehab:ahmed@quiz-hvhc1.gcp.mongodb.net/test?retryWrites=true
+//mongodb+srv://AhmedEhab:ahmed123@quizcluster-c3eyb.mongodb.net/test?retryWrites=true
