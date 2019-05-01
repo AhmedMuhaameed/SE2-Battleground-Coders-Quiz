07e7ab1a34d87a5f2c21eb4849db873bef6762b5
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2019-05-01 17:16:15 +0200

    add more testcases

--- test/test.js
+++ test/test.js
@@ -1,25 +1,58 @@
-var assert = require('assert');
+const model = require('../model/quiz');
+const modelDB = require('../model/quizDB');
+const mongoose = require('mongoose');
+const key = require('../config');
 
-const Quiz = require('../model/quiz');
-const QuizDB = require('../model/quizDB');
 
-const quiz = new Quiz(QuizDB);
-
-describe('Get Quiz', () => {
-    //let quiz = require('../model/quiz');
-    it('should return null if no value', () => {
-        let test = quiz.getQuizJson();
-        //console.log(test);
-        assert.notEqual(test, null);
+test('connect to database', async () =>{
+    await mongoose
+    .connect(key.mongoURI, {useNewUrlParser: true})
+    .then(() => {
+        console.log(`connected to database`)
     })
+    .catch(err => {
+        console.log(err)
+    })
+
+    mongoose.Promise = global.Promise
 })
 
-describe('Get Quizzes', () => {
-  //let quiz = require('../model/quiz');
-  it('should return null if no value', () => {
-      let test = quiz.getQuizzesJson();
-      //console.log(test);
-      assert.notEqual(test, null);
-  })
+const db = new model(modelDB);
+
+test('addQuizJson', async () => {
+
+    // change quiz
+
+    let newQuiz = {
+        "size": 5,
+        "question": ["How many degrees are in a triangle?","The Pythagorean theorem holds true for which type of triangle?","What is the biggest angle?","A polygon is a figure that is closed and has straight lines. Is this a polygon?","C=2*Pi*r (Circumference of a Circle)"
+        ],
+        "choices": [["90","360","180"],["All triangles","Right angled triangle","Isosceles triangle"],["Right","Acute","Obtuse"],["yes","no"],["True","False"]],
+        "answer": [[180],["Right angled triangle"],["Obtuse"],["yes"],["True"]],
+        "field": "Geometry"
+    }
+
+    let quiz = await db.addQuizJson(newQuiz);
+    expect(quiz).not.toBeNull();
 })
 
+test('getQuizJson', async ()=>{
+    //expect.assertions(1);
+    let quiz = await db.getQuizJson();
+    expect(quiz).not.toBeNull();
+})
+
+test('getQuizJson', async ()=>{
+    //expect.assertions(1);
+    let quizzes = await db.getQuizzesJson();
+    expect(quizzes).not.toBeNull();
+})
+
+test('delete all data', async () => {
+    let res = await modelDB.deleteMany({});
+    expect(res).not.toBeNull();
+})
+
+test('close connection with mongo db', async () => {
+    await mongoose.disconnect();
+})
